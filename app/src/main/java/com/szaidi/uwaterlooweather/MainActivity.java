cd0ac3c50d8ec82801968633aa356bdda12e65ab
TheSZ11 <sajjadzaidi11@gmail.com>
2015-02-19 10:52:36 -0500

    Finalised windchill logic
    Finished up windchill logic, implemented swipe to refresh

---
+++ app/src/main/java/com/szaidi/uwaterlooweather/MainActivity.java
@@ -0,0 +1,259 @@
+package com.szaidi.uwaterlooweather;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	
+	Context context;
+	String feedUrl = "https://api.uwaterloo.ca/v2/weather/current.json";
+	
+	String temp;
+	String observationTime;
+	String windchill;
+	String hourMaxTemp;
+	String hourMinTemp;
+	String windSpeed;
+	String windDirection;
+	String observationDate;
+	String date;
+	String Time;
+	
+	
+	TextView textView;
+	TextView textViewObservationDate;
+	TextView textViewObservationTime;
+	TextView textViewWindchill;
+	TextView textViewHourMaxTemp;
+	TextView textViewHourMinTemp;
+	TextView textViewWindSpeed;
+	TextView textViewWindDirection;
+
+    SwipeRefreshLayout swipeLayout;
+    WeatherListTask loaderTask;
+
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_main);
+		
+		context = this;
+
+		loaderTask = new WeatherListTask();
+		loaderTask.execute();
+
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        swipeLayout.setColorSchemeColors(Color.BLACK, Color.YELLOW);
+
+        swipeLayout.setOnRefreshListener(onRefreshListener);
+
+
+        textView = (TextView) findViewById(R.id.textView);
+		textViewObservationDate = (TextView) findViewById(R.id.textViewObservationDate);
+		textViewObservationTime = (TextView) findViewById(R.id.textViewObservationTime);
+		textViewWindchill = (TextView) findViewById(R.id.textViewWindchill);
+		textViewHourMaxTemp = (TextView) findViewById(R.id.textViewHourMaxTemp);
+		textViewHourMinTemp= (TextView) findViewById(R.id.textViewHourMinTemp);
+		textViewWindSpeed= (TextView) findViewById(R.id.textViewWindSpeed);
+		textViewWindDirection = (TextView) findViewById(R.id.textViewWindDirection);
+		
+		
+	}
+
+    OnRefreshListener onRefreshListener = new OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            WeatherListTask refreshTask = new WeatherListTask();
+            refreshTask.execute();
+            swipeLayout.setRefreshing(false);
+        }
+    };
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public class WeatherListTask extends AsyncTask<Void, Void, Void >{
+
+		ProgressDialog dialog;
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			dialog = new ProgressDialog(context);
+			dialog.setTitle("Connecting to satellites...");
+			dialog.show();
+			super.onPreExecute();
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			HttpClient client = new DefaultHttpClient();
+			HttpGet getRequest = new HttpGet(feedUrl);
+			
+			try {
+				HttpResponse responce = client.execute(getRequest);
+				StatusLine statusLine = responce.getStatusLine();
+				int statusCode = statusLine.getStatusCode();
+				
+				if(statusCode != 200){
+					
+					return null;
+				}
+				
+				InputStream jsonStream = responce.getEntity().getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(jsonStream));
+				StringBuilder builder = new StringBuilder();
+				String line;
+				while((line = reader.readLine()) != null){
+					
+					builder.append(line);
+				}
+				
+				String jsonData = builder.toString();
+				
+				JSONObject json = new JSONObject(jsonData);
+				JSONObject weather = json.getJSONObject("data");
+				temp = weather.getString("temperature_current_c");
+				observationTime = weather.getString("observation_time");
+				windchill = weather.getString("windchill_c");
+				hourMaxTemp = weather.getString("temperature_24hr_max_c");
+				hourMinTemp = weather.getString("temperature_24hr_min_c");
+				windSpeed = weather.getString("wind_speed_kph");
+				windDirection = weather.getString("wind_direction_degrees");
+				
+				
+				SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd");
+				SimpleDateFormat output = new SimpleDateFormat("dd MMM yyyy");
+				try {
+				    Date Date = input.parse(observationTime);                 // parse input 
+				    date = (output.format(Date));    // format output
+				} catch (ParseException e) {
+				    e.printStackTrace();
+				}
+		
+				SimpleDateFormat fmt = new SimpleDateFormat("HH:mm:ss");
+				Date time = new Date();
+				Time = fmt.format(time);
+				
+				temp = temp + "°c";
+				hourMaxTemp = "high of " + hourMaxTemp + "°c";
+				hourMinTemp = "low of " + hourMinTemp+ "°c";
+
+                int windSpeedInt = Integer.valueOf(windSpeed);
+                
+                if(windSpeedInt == 0){
+                    windchill = temp;
+                }
+
+				windchill = "feels like " + windchill+ "°c";
+				windSpeed = windSpeed + "kph";
+
+                int windDirectionInt = Integer.valueOf(windDirection);
+
+                if (348.75 <= windDirectionInt && windDirectionInt <= 360) {
+                    windDirection = "N";
+                } else if (0 <= windDirectionInt && windDirectionInt <= 11.25) {
+                    windDirection = "N";
+                } else if (11.25 < windDirectionInt && windDirectionInt <= 33.75) {
+                    windDirection = "NNE";
+                } else if (33.75 < windDirectionInt && windDirectionInt <= 56.25) {
+                    windDirection = "NE";
+                } else if (56.25 < windDirectionInt && windDirectionInt <= 78.75) {
+                    windDirection = "ENE";
+                } else if (78.75 < windDirectionInt && windDirectionInt <= 101.25) {
+                    windDirection = "E";
+                } else if (101.25 < windDirectionInt && windDirectionInt <= 123.75) {
+                    windDirection = "ESE";
+                } else if (123.75 < windDirectionInt && windDirectionInt <= 146.25) {
+                    windDirection = "SE";
+                } else if (146.25 < windDirectionInt && windDirectionInt <= 168.75) {
+                    windDirection = "SSE";
+                } else if (168.75 < windDirectionInt && windDirectionInt <= 191.25) {
+                    windDirection = "S";
+                } else if (191.25 < windDirectionInt && windDirectionInt <= 213.75) {
+                    windDirection = "SSW";
+                } else if (213.75 < windDirectionInt && windDirectionInt <= 236.25) {
+                    windDirection = "SW";
+                } else if (236.25 < windDirectionInt && windDirectionInt <= 258.75) {
+                    windDirection = "WSW";
+                } else if (258.75 < windDirectionInt && windDirectionInt <= 281.25) {
+                    windDirection = "W";
+                } else if (281.25 < windDirectionInt && windDirectionInt <= 303.75) {
+                    windDirection = "WNW";
+                } else if (303.75 < windDirectionInt && windDirectionInt <= 326.25) {
+                    windDirection = "NW";
+                } else if (326.25 < windDirectionInt && windDirectionInt < 348.75) {
+                    windDirection = "NNW";
+                } else {
+                    windDirection = "-";
+                }
+				
+			} catch (ClientProtocolException e) {
+				
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			// TODO Auto-generated method stub
+			dialog.dismiss();
+	
+			super.onPostExecute(result);
+			
+			textView.setText(date);
+			textViewObservationDate.setText(Time);
+			textViewObservationTime.setText(hourMaxTemp);
+			textViewWindchill.setText(temp);
+			textViewHourMaxTemp.setText(hourMinTemp);
+			textViewHourMinTemp.setText(windchill);
+			textViewWindSpeed.setText(windSpeed);
+			textViewWindDirection.setText(windDirection);
+			}
+		
+	}
+	
+}
