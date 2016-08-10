b1008f4d93a2db6ac38843e28494fe13ecbfb314
Sajjad Zaidi <sajjadzaidi11@gmail.com>
2016-08-10 01:48:26 -0400

    SZ - *NEW*Fixed API issues, changed package name, brand new listing.

--- app/build/generated/source/buildConfig/debug/com/szaidi/uwaterlooweather/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/szaidi/uwaterlooweather/BuildConfig.java
@@ -5,9 +5,9 @@ package com.szaidi.uwaterlooweather;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.szaidi.uwaterlooweather";
+  public static final String APPLICATION_ID = "com.szaidi.uwaterlooweather2";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 3;
-  public static final String VERSION_NAME = "";
+  public static final String VERSION_NAME = "1.2";
 }
